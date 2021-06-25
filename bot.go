689b1dbdfb976e76f05a2ad2163020d0b3f7dd35
jspc <james@zero-internet.org.uk>
2021-06-25 11:27:15 +0900

    Split timer logic, use named functions per call, introduce default time with custom message

--- bot.go
+++ bot.go
@@ -20,6 +20,7 @@ const (
   $brun timer for 10m                            - Run a timer which sends "Time to relax!" after 10 minutes
                                                  The 10m can be pretty much anything, like 5m, or 30s or 12h30m14s
                                                  Note the lack of spaces. See: https://golang.org/pkg/time/#ParseDuration
+  $brun timer and say "Wake up!"                 - Run a 25 minute timer, with the message "Wake up!"
   $brun timer for 10m and say "Wake up!"         - Run a timer which sends "Wake up!" after 10 minutes.
                                                  The message can be pretty much anything, so long as it's between two
                                                  quotation marks.
@@ -48,9 +49,10 @@ func New(user, password, server string, verify bool) (b Bot, err error) {
 
 	router := bottom.NewRouter()
 	router.AddRoute(`(?i)^help$`, b.help)
-	router.AddRoute(`(?i)^run\s+timer$`, b.timer)
-	router.AddRoute(`(?i)^run\s+timer\s+for\s+(\S*)$`, b.timer)
-	router.AddRoute(`(?i)^run\s+timer\s+for\s+(\S*)\s+and\s+say\s+\"(.+)\"$`, b.timer)
+	router.AddRoute(`(?i)^run\s+timer$`, b.defaultTimer)
+	router.AddRoute(`(?i)^run\s+timer\s+for\s+(\S*)$`, b.durationTimer)
+	router.AddRoute(`(?i)^run\s+timer\s+and\s+say\s+\"(.+)\"$`, b.msgTimer)
+	router.AddRoute(`(?i)^run\s+timer\s+for\s+(\S*)\s+and\s+say\s+\"(.+)\"$`, b.fullyCustomisedTimer)
 
 	b.bottom.Middlewares.Push(router)
 
@@ -65,19 +67,8 @@ func (b Bot) help(sender, _ string, _ []string) (err error) {
 	return
 }
 
-func (b Bot) timer(sender, channel string, groups []string) (err error) {
+func (b Bot) timer(sender, channel, duration, msg string) (err error) {
 	b.bottom.Client.Cmd.Messagef(channel, "Creating timer for %s", sender)
-
-	msg := DefaultMessage
-	duration := DefaultDuration
-
-	if len(groups) >= 2 {
-		duration = groups[1]
-
-		if len(groups) == 3 {
-			msg = groups[2]
-		}
-	}
 
 	d, err := time.ParseDuration(duration)
 	if err != nil {
@@ -87,4 +78,21 @@ func (b Bot) timer(sender, channel string, groups []string) (err error) {
 	go RunTimer(b.bottom.Client, sender, channel, msg, d)
 
 	return
+
+}
+
+func (b Bot) defaultTimer(sender, channel string, _ []string) (err error) {
+	return b.timer(sender, channel, DefaultDuration, DefaultMessage)
+}
+
+func (b Bot) durationTimer(sender, channel string, groups []string) (err error) {
+	return b.timer(sender, channel, groups[1], DefaultMessage)
+}
+
+func (b Bot) msgTimer(sender, channel string, groups []string) (err error) {
+	return b.timer(sender, channel, DefaultDuration, groups[1])
+}
+
+func (b Bot) fullyCustomisedTimer(sender, channel string, groups []string) (err error) {
+	return b.timer(sender, channel, groups[1], groups[2])
 }
